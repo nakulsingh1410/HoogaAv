74271080a1b0d74c5a6e9aa69589d2884b27327e
Amrendra <nakulsingh1410@gmail.com>
2018-02-04 01:04:57 +0530

    payment UI design

--- Hooga/AppModules/PaymentModule/ViewControler/PaymentVC.swift
+++ Hooga/AppModules/PaymentModule/ViewControler/PaymentVC.swift
@@ -22,14 +22,21 @@ class PaymentVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
+  
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension PaymentVC : UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
 
+}
+
+extension PaymentVC : UICollectionViewDelegate{
+    
+    
 }
---
+++ Hooga/AppModules/PaymentModule/ViewControler/QcodeTicketVC.swift
@@ -0,0 +1,35 @@
+//
+//  QcodeTicketVC.swift
+//  Hooga
+//
+//  Created by @mrendra on 04/02/18.
+//  Copyright © 2018 Nakul Singh. All rights reserved.
+//
+
+import UIKit
+
+class QcodeTicketVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
